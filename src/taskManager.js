fe32296ba105d19118b82e3e47a9599c4f25ac3f
Amanda Soares <amanda.soares2@iesb.edu.br>
2026-04-16 09:39:54 -0300

    green: implementa filterTasks all

--- src/taskManager.js
+++ src/taskManager.js
@@ -1,3 +1,9 @@
 export function removeTask(tasks, taskId) {
   return tasks.filter(task => task.id !== taskId)
 }
+
+export function filterTasks(tasks, status) {
+  if (status === 'all') {
+    return tasks
+  }
+}
